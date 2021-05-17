cc1f37db703cc5355df79bc4eb45105a42d7eaa2
Hong <doublebeet@gmail.com>
2021-05-17 04:44:48 +0000

    Upload gts modules and utils

---
+++ libs/tools/gts_utils.py
@@ -0,0 +1,194 @@
+import math
+import torch
+import torch.optim
+import torch.nn.functional as f
+import time
+import copy
+from torch.nn import functional
+
+
+class Beam:  # the class save the beam node
+    def __init__(self, score, input_var, hidden, all_output):
+        self.score = score
+        self.input_var = input_var
+        self.hidden = hidden
+        self.all_output = all_output
+
+
+def time_since(s):  # compute time
+    m = math.floor(s / 60)
+    s -= m * 60
+    h = math.floor(m / 60)
+    m -= h * 60
+    return '%dh %dm %ds' % (h, m, s)
+
+
+def generate_tree_input(target, decoder_output, nums_stack_batch, num_start, unk):
+    # when the decoder input is copied num but the num has two pos, chose the max
+    target_input = copy.deepcopy(target)
+    for i in range(len(target)):
+        if target[i] == unk:
+            num_stack = nums_stack_batch[i].pop()
+            max_score = -float("1e12")
+            for num in num_stack:
+                if decoder_output[i, num_start + num] > max_score:
+                    target[i] = num + num_start
+                    max_score = decoder_output[i, num_start + num]
+        if target_input[i] >= num_start:
+            target_input[i] = 0
+    return torch.LongTensor(target), torch.LongTensor(target_input)
+
+
+
+def get_all_number_encoder_outputs(encoder_outputs, num_pos, batch_size, num_size, hidden_size):
+    
+    indices = list()
+    sen_len = encoder_outputs.size(0)
+    masked_index = []
+    temp_1 = [1 for _ in range(hidden_size)]
+    temp_0 = [0 for _ in range(hidden_size)]
+
+    for b in range(batch_size):
+        for i in num_pos[b]:
+            indices.append(i + b * sen_len)
+            masked_index.append(temp_0)
+        indices += [0 for _ in range(len(num_pos[b]), num_size)]
+        masked_index += [temp_1 for _ in range(len(num_pos[b]), num_size)]
+    indices = torch.LongTensor(indices).to(device=encoder_outputs.device)
+    masked_index = torch.BoolTensor(masked_index).to(
+        device=encoder_outputs.device)
+    masked_index = masked_index.view(batch_size, num_size, hidden_size)
+
+    all_outputs = encoder_outputs.transpose(0, 1).contiguous()
+    # S x B x H -> (B x S) x H
+    all_embedding = all_outputs.view(-1, encoder_outputs.size(2))
+    all_num = all_embedding.index_select(0, indices)
+    all_num = all_num.view(batch_size, num_size, hidden_size)
+    return all_num.masked_fill_(masked_index, 0.0)
+
+
+
+def copy_list(l):
+    r = []
+    if len(l) == 0:
+        return r
+    for i in l:
+        if type(i) is list:
+            r.append(copy_list(i))
+        else:
+            r.append(i)
+    return r
+
+
+class TreeNode:  # the class save the tree node
+    def __init__(self, embedding, left_flag=False):
+        self.embedding = embedding
+        self.left_flag = left_flag
+
+
+
+class TreeBeam:  # the class save the beam node
+    def __init__(self, score, node_stack, embedding_stack, left_childs, out):
+        self.score = score
+        self.embedding_stack = copy_list(embedding_stack)
+        self.node_stack = copy_list(node_stack)
+        self.left_childs = copy_list(left_childs)
+        self.out = copy.deepcopy(out)
+
+
+class TreeEmbedding:  # the class save the tree
+    def __init__(self, embedding, terminal=False, op_type=None):
+        self.embedding = embedding
+        self.terminal = terminal
+        self.op_type = op_type
+        
+        
+def sequence_mask(sequence_length, max_len=None):
+    if max_len is None:
+        max_len = sequence_length.data.max()
+    batch_size = sequence_length.size(0)
+    seq_range = torch.arange(0, max_len).long()
+    seq_range_expand = seq_range.unsqueeze(0).expand(batch_size, max_len)
+    if sequence_length.is_cuda:
+        seq_range_expand = seq_range_expand.cuda()
+    seq_length_expand = (sequence_length.unsqueeze(
+        1).expand_as(seq_range_expand))
+    return seq_range_expand < seq_length_expand
+
+
+def masked_cross_entropy(logits, target, length):
+    """
+    Args:
+        logits: A Variable containing a FloatTensor of size
+            (batch, max_len, num_classes) which contains the
+            unnormalized probability for each class.
+        target: A Variable containing a LongTensor of size
+            (batch, max_len) which contains the index of the true
+            class for each corresponding step.
+        length: A Variable containing a LongTensor of size (batch,)
+            which contains the length of each data in a batch.
+    Returns:
+        loss: An average loss value masked by the length.
+    """
+    length = torch.LongTensor(length).to(device=logits.device)
+
+    # logits_flat: (batch * max_len, num_classes)
+    logits_flat = logits.view(-1, logits.size(-1))
+    # log_probs_flat: (batch * max_len, num_classes)
+    log_probs_flat = functional.log_softmax(logits_flat, dim=1)
+    # target_flat: (batch * max_len, 1)
+    target_flat = target.view(-1, 1)
+    # losses_flat: (batch * max_len, 1)
+    losses_flat = -torch.gather(log_probs_flat, dim=1, index=target_flat)
+
+    # losses: (batch, max_len)
+    losses = losses_flat.view(*target.size())
+    # mask: (batch, max_len)
+    mask = sequence_mask(sequence_length=length, max_len=target.size(1))
+    losses = losses * mask.float()
+    loss = losses.sum() / length.float().sum()
+    # if loss.item() > 10:
+    #     print(losses, target)
+    return loss
+
+
+def masked_cross_entropy_without_logit(logits, target, length):
+    if torch.cuda.is_available():
+        length = torch.LongTensor(length).cuda()
+    else:
+        length = torch.LongTensor(length)
+    """
+    Args:
+        logits: A Variable containing a FloatTensor of size
+            (batch, max_len, num_classes) which contains the
+            unnormalized probability for each class.
+        target: A Variable containing a LongTensor of size
+            (batch, max_len) which contains the index of the true
+            class for each corresponding step.
+        length: A Variable containing a LongTensor of size (batch,)
+            which contains the length of each data in a batch.
+    Returns:
+        loss: An average loss value masked by the length.
+    """
+
+    # logits_flat: (batch * max_len, num_classes)
+    logits_flat = logits.view(-1, logits.size(-1))
+
+    # log_probs_flat: (batch * max_len, num_classes)
+    log_probs_flat = torch.log(logits_flat + 1e-12)
+
+    # target_flat: (batch * max_len, 1)
+    target_flat = target.view(-1, 1)
+    # losses_flat: (batch * max_len, 1)
+    losses_flat = -torch.gather(log_probs_flat, dim=1, index=target_flat)
+
+    # losses: (batch, max_len)
+    losses = losses_flat.view(*target.size())
+
+    # mask: (batch, max_len)
+    mask = sequence_mask(sequence_length=length, max_len=target.size(1))
+    losses = losses * mask.float()
+    loss = losses.sum() / length.float().sum()
+    # if loss.item() > 10:
+    #     print(losses, target)
+    return loss
